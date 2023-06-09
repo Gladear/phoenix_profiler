a25fa4da7729d871169f2ae2c082d61f3293f0c8
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2023-06-09 12:28:12 +0100

    refactor: simplify JavaScript greatly

--- priv/static/toolbar.js
+++ priv/static/toolbar.js
@@ -1,47 +1,17 @@
-(function() {
-  var SHOW = "phxprof-toolbar-show";
+const SHOW = "phxprof-toolbar-show";
 
-  // Browser
-  // Copyright (c) 2018 Chris McCord
-  // https://github.com/phoenixframework/phoenix_live_view/blob/2ea98193420d236cbd5797e0cd258233c4dff5a7/assets/js/phoenix_live_view/browser.js
-  var Browser = {
-    setLocal(localStorage, namespace, subkey, value) {
-      var key = this.localKey(namespace, subkey)
-      localStorage.setItem(key, JSON.stringify(value))
-      return value
-    },
-    getLocal(localStorage, namespace, subkey, defaultVal) {
-      var value = JSON.parse(localStorage.getItem(this.localKey(namespace, subkey)))
-      return value === null ? defaultVal : value
-    },
-    localKey(namespace, subkey) {
-      return `${namespace}-${subkey}`
-    },
-    show(localStorage) {
-      this.setLocal(localStorage, window.location.host, SHOW, true)
-      toolbar.classList.remove("miniaturized")
-    },
-    hide(localStorage) {
-      this.setLocal(localStorage, window.location.host, SHOW, false)
-      toolbar.classList.add("miniaturized")
-    },
-    showOrHide(localStorage) {
-      (this.getLocal(localStorage, window.location.host, SHOW, true)) ? this.show(localStorage): this.hide(localStorage)
-    }
-  };
+const toolbar = document.querySelector(".phxprof-toolbar");
 
-  var localStorage = window.localStorage;
+function toggleToolbar(open) {
+  toolbar.classList.toggle("miniaturized", !open);
+  localStorage.setItem(SHOW, String(open));
+}
 
-  var showToolbar = function() { Browser.show(localStorage); };
-  var hideToolbar = function() { Browser.hide(localStorage); };
+toolbar
+  .querySelector(".show-button")
+  .addEventListener("click", () => toggleToolbar(true));
+toolbar
+  .querySelector(".hide-button")
+  .addEventListener("click", () => toggleToolbar(false));
 
-  var toolbar = document.querySelector(".phxprof-toolbar");
-
-  var showButton = toolbar.querySelector("button.show-button");
-  showButton.addEventListener("click", showToolbar);
-
-  var hideButton = toolbar.querySelector("button.hide-button");
-  hideButton.addEventListener("click", hideToolbar);
-
-  Browser.showOrHide(localStorage);
-})();
+toggleToolbar(localStorage.getItem(SHOW) === "true");
